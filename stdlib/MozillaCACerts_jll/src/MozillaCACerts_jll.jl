8f138e81cfb26f802aec3de5b7046184ab55e852
Stefan Karpinski <stefan@karpinski.org>
2020-09-01 19:05:14 -0400

    add dummy MozillaCACerts_jll stdlib

---
+++ stdlib/MozillaCACerts_jll/src/MozillaCACerts_jll.jl
@@ -0,0 +1,9 @@
+# This file is a part of Julia. License is MIT: https://julialang.org/license
+
+## dummy stub for https://github.com/JuliaBinaryWrappers/MozillaCACerts_jll.jl
+
+module MozillaCACerts_jll
+
+const cacert = normpath(Sys.BINDIR, Base.DATAROOTDIR, "julia", "cert.pem")
+
+end
