9e1ac2eaa9c2f144eee838b8ea00ac395470b21b
Stefan Karpinski <stefan@karpinski.org>
2020-09-08 16:34:25 -0400

    wip

--- stdlib/LibCURL_jll/test/runtests.jl
+++ stdlib/LibCURL_jll/test/runtests.jl
@@ -2,6 +2,6 @@ using Test
 using LibCURL_jll
 
 @testset "LibCURL_jll" begin
-    v = unsafe_string(ccall((:curl_version, :libcurl), Cstring, ()))
+    v = unsafe_string(ccall((:curl_version, libcurl), Cstring, ()))
     @test startswith(v, "libcurl/")
 end
